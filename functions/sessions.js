781b08dc33ffbdcbfbd3eda0d49ca80ade2e292e
nevosangkane446 <nevosangkane446@gmail.com>
2024-06-06 08:08:24 +0000

    first commit

---
+++ functions/sessions.js
@@ -0,0 +1,59 @@
+const wconnect = require("@wppconnect-team/wppconnect");
+
+function start(client) {
+    client.onMessage((message) => {
+      if (message.body === 'Hello') {
+        client
+          .sendText(message.from, 'Hello, how I may help you?')
+          .then((result) => {
+            console.log('Result: ', result); //return object success
+          })
+          .catch((erro) => {
+            console.error('Error when sending: ', erro); //return object error
+          });
+      }
+
+    }
+  );
+}
+
+function createSes() {
+    const sessionPath = './session';
+return wconnect
+.create({
+  session: 'client1',
+  statusFind: (statusSession, session) => {
+    if (statusSession === 'serverClose') {
+      console.log('Server closed');
+    }
+    if (statusSession === 'deleteToken') {
+       createSes();
+    }
+    if (statusSession == "isLogged") console.log("Logged")
+    if (statusSession == "qrReadSuccess") console.log("QR Code read")
+    // return: isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken
+    console.log('Status Session: ', statusSession);
+    // create session wss return "serverClose" case server for close
+    console.log('Session name: ', session);
+  },
+  puppeteerOptions: {
+    args: [
+        `--user-data-dir=${sessionPath}`,
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--disable-gpu'
+    ],
+    protocolTimeout: 160000 // Set timeout to 60 seconds
+},
+})
+.then((client) => {
+    start(client)
+    return client
+})
+.catch((error) => console.log(error));
+
+}
+
+module.exports = createSes;
---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const path = require('path');
+const createSes = require('./functions/sessions');
+const fs = require("fs");
+
+
+const app = express();
+const sessionPath = path.join(__dirname, 'tokens', "client1");
+const lockFilePath = path.join(sessionPath, 'SingletonLock');
+if (fs.existsSync(lockFilePath)) {
+    fs.rm(lockFilePath);
+    console.log("Deleted...")
+}
+
+const sock = createSes();
+let client;
+sock.then((c) => (client = c))
+if (sock) console.log(sock)
+
+
+const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
+
+app.use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/api', (req, res) => {
+  res.json({"msg": "Hello world"});
+});
+
+app.get("/alok", (req, res) => {
+  res.render("alok");
+})
+app.get("/api/:number/:msg", async (req, res) => {
+  const {number, msg} = req.params;
+  if (!number || !msg) return res.json("error no body")
+  console.log(number, msg)
+  await client
+  .sendText(number+'@c.us', msg)
+  .then((result) => {
+    console.log('Result: ', result); //return object success
+    res.json("success")
+  })
+  .catch((erro) => {
+    console.error('Error when sending: ', erro); //return object error
+    res.json("error")
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+})
